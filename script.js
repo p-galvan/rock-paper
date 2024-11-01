99c30e66609328613e920bbcd2e93efe6ca13f61
p-galvan <p.galvant@gmail.com>
2024-11-01 14:09:02 -0700

    feat: create playgame function

--- script.js
+++ script.js
@@ -29,46 +29,56 @@ function getHumanChoice () {
     return answer;
 }
 
-function playRound (humanc, computerc) {
 
-    if (humanc === computerc) {
-        return console.log("tie, play again!");
-    }
 
-    else if (humanc === "rock" && computerc === "scissors") {
-        humanScore++;
-        return console.log("you win!");
-    }
+function playGame() {
+    // Declare score and round variables
+    let humanScore = 0;
+    let computerScore = 0;
+    let totalRounds = 0;
+    
+    function playRound (humanc, computerc) {
 
-    else if (humanc === "paper" && computerc === "rock") {
-        humanScore++;
-        return console.log("you win!");
+        if (humanc === computerc) {
+            return console.log("tie, play again!");
+        }
+    
+        else if (humanc === "rock" && computerc === "scissors") {
+            humanScore++;
+            return console.log("you win!");
+        }
+    
+        else if (humanc === "paper" && computerc === "rock") {
+            humanScore++;
+            return console.log("you win!");
+        }
+    
+        else if (humanc === "scissors" && computerc === "paper") {
+            humanScore++;
+            return console.log("you win!");
+        }
+    
+        else {
+            computerScore++;
+            return console.log("you lose!");
+        }
+            
     }
-
-    else if (humanc === "scissors" && computerc === "paper") {
-        humanScore++;
-        return console.log("you win!");
+    
+    while (totalRounds < 5) {
+        playRound(humanChoice, computerChoice);
+        totalRounds++;
     }
-
-    else {
-        computerScore++;
-        return console.log("you lose!");
-    }
-        
 }
-
 // Call functions to generate random computer choice and get human choice
-computerChoice = getComputerChoice ();
-humanChoice = getHumanChoice ();
+const computerChoice = getComputerChoice ();
+const humanChoice = getHumanChoice ();
 
 console.log("computer choice: " + computerChoice);
 console.log("human choice: " + humanChoice);
 
-// Declare score variables
-let humanScore = 0;
-let computerScore = 0;
 
-playRound(humanChoice, computerChoice);
+// playRound(humanChoice, computerChoice);
 
 console.log("your score: " + humanScore);
 console.log("computer score: " + computerScore);
