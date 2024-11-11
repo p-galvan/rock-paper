35cd308489dadc1b2613bb0ebe797c6c335d7caf
p-galvan <p.galvant@gmail.com>
2024-11-11 13:03:24 -0800

    feat: update UI logic to display choices

--- script.js
+++ script.js
@@ -16,98 +16,118 @@ function getComputerChoice () {
     return choice;
 }
 
-function getHumanChoice () {
-    // Prompt user for selection of rock/paper/scissors
-    answer = prompt("Enter rock, paper or scissors: ");
-    answer = answer.toLowerCase();
 
-    // Validate user input, reprompt if user enters invalid input
-    if (answer != "rock" && answer != "paper" && answer != "scissors") {
-        alert("invalid input, please try again typing rock, paper or scissors");
-        return getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return console.log("tie!");
     }
 
-    return answer;
+    else if (humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore++;
+        return console.log("you win!");
+    }
+
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
+        return console.log("you win!");
+    }
+
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        return console.log("you win!");
+    }
+
+    else {
+        computerScore++;
+        return console.log("you lose!");
+    }       
 }
 
+// Update UI after each round
+function updateChoices(humanChoice, computerChoice) {
+    let choices = document.querySelector("#choices");
+
+    let computerChoiceDisplay = document.createElement("span");
+    computerChoiceDisplay.textContent = computerChoice
+
+    let humanChoiceDisplay = document.createElement("span");
+    humanChoiceDisplay.textContent = humanChoice;
+
+    choices.appendChild(humanChoiceDisplay);
+    choices.appendChild(computerChoiceDisplay);
+
+}
 function playGame() {
 
     // Declare variables to keep track of scores, rounds and result
     let computerScore = 0;
     let humanScore = 0;
-    let totalRounds = 0;
+    let humanChoice = "";
     let result = "";
     let gameInfo = document.querySelector("#game_info");
     let choices = document.querySelector("#choices");
-    let scores = document.querySelector("scores");
+    let scores = document.querySelector("#scores");
     
-    function playRound (humanChoice, computerChoice) {
-        if (humanChoice === computerChoice) {
-            return console.log("tie!");
+    
+    // Event listeners for player selection buttons
+    let btn = document.querySelector("#choiceBtns");
+    btn.addEventListener("click", (event) => {
+        if (event.target.id === "rockBtn") {
+            humanChoice = "rock";
+            computerChoice = getComputerChoice();
+
+            playRound(humanChoice, computerChoice);
         }
-    
-        else if (humanChoice === "rock" && computerChoice === "scissors") {
-            humanScore++;
-            return console.log("you win!");
+        else if (event.target.id === "paperBtn") {
+            humanChoice = "paper";
+            computerChoice = getComputerChoice();
+
+            playRound(humanChoice, computerChoice);
         }
-    
-        else if (humanChoice === "paper" && computerChoice === "rock") {
-            humanScore++;
-            return console.log("you win!");
-        }
-    
-        else if (humanChoice === "scissors" && computerChoice === "paper") {
-            humanScore++;
-            return console.log("you win!");
-        }
-    
         else {
-            computerScore++;
-            return console.log("you lose!");
-        }       
-    }
-    
+            humanChoice = "scissors";
+            computerChoice = getComputerChoice();
+
+            playRound(humanChoice, computerChoice);
+        }
+    });
+
+
+
     // Play the game until a player reaches 6 points
-    while (computerScore < 6 || humanScore < 6) {
-        // Get user input before round
-        computerChoice = getComputerChoice();
-        humanChoice = getHumanChoice();
-        
-        // Update computer and human choices in UI
-        const computerChoiceDisplay = document.createElement("span");
-        computerChoiceDisplay.textContent = "Computer choice: " + computerChoice;
-        console.log("computer choice: " + computerChoice);
+    
+    // Update computer and human choices in UI
+    const computerChoiceDisplay = document.createElement("span");
+    computerChoiceDisplay.textContent = "Computer choice: " + computerChoice;
+    console.log("computer choice: " + computerChoice);
 
-        const humanChoiceDisplay = document.createElement("span");
-        humanChoiceDisplay.textContent = "Human choice: " + humanChoice;
-        console.log("human choice: " + humanChoice);
+    const humanChoiceDisplay = document.createElement("span");
+    humanChoiceDisplay.textContent = "Human choice: " + humanChoice;
+    console.log("human choice: " + humanChoice);
 
-        scores.appendChild(computerChoiceDisplay);
-        scores.appendChild(humanChoiceDisplay);
+    scores.appendChild(computerChoiceDisplay);
+    scores.appendChild(humanChoiceDisplay);
 
-        // Play round
-        playRound(humanChoice, computerChoice);
-        totalRounds++;
 
-        // Log scores and round after every round
-        console.log("human score: " + humanScore);
-        console.log("computer score: " + computerScore);
-        console.log("total rounds: " + totalRounds);
-    }
+    // Log scores and round after every round
+    console.log("human score: " + humanScore);
+    console.log("computer score: " + computerScore);
+    console.log("total rounds: " + totalRounds);
 
-    // Return winner
-    if (humanScore > computerScore) {
-        result = "you win the game!";
-        return result;
-    } 
-    else if (humanScore < computerScore){
-        result = "you lose the game!";
-        return result;
-    }
-    else {
-        result = "TIE!";
-        return result;
-    }
+
+    // // Return winner
+    // if (humanScore > computerScore) {
+    //     result = "you win the game!";
+    //     return result;
+    // } 
+    // else if (humanScore < computerScore){
+    //     result = "you lose the game!";
+    //     return result;
+    // }
+    // else {
+    //     result = "TIE!";
+    //     return result;
+    // }
 
 }
 
